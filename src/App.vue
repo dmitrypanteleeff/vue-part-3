d7770455dd559ff119d1ee76959154742d071f15
unknown <dmitrypanteleeff@yandex.ru>
2024-12-16 13:08:01 +0300

    lesson 25 - add scss

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
-  <h3>Hello</h3>
+  <div class="wrapper">
+    <HeaderComponent />
+    <div class="wrapper-content">
+      <div class="container">
+        <h1>Hello</h1>
+        <p>hello</p>
+      </div>
+    </div>
+    <FooterComponent></FooterComponent>
+  </div>
 </template>
 
 <script>
 
-export default {}
+import HeaderComponent from '@/components/header-component.vue';
+import FooterComponent from '@/components/footer-component.vue';
+
+export default {
+  components: {
+    HeaderComponent, FooterComponent
+  }
+}
 </script>
 
 <style lang="scss">
